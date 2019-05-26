73e2f037960736d56c52efe760c2c57bf0216bd0
Napwob <47683903+Napwob@users.noreply.github.com>
2019-05-26 13:26:55 +0300

    Update string.c

--- string.c
+++ string.c
@@ -1,27 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "string.h"
 #define DELIM "."
 #define MAX_PATH 260
-
-void input(char *delim);
-char *sstr(char *string1, const char *string2);
-char* mystrchr(const char* s, const char c);
-int procces(char delim,char* paths);
-int check(char* paths);
-int chs(char *paths);
-int csim(char *paths);
-int cdom(char* paths);
-int cip(char* paths);
-int is_valid_ip(char* ip_str);
-int valid_digit(char* ip_str);
-void op(char *paths);
-int slen(char *str);
-int ati(char* s);
-int sspn(char *str, const char *substr);
-int scmp(const char *str, const char *strc);
-char *scpy(char *des, const char *src);
-char* stok(char* str, const char* delim);
 
 void op(char *paths)
 {
