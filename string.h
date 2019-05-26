1f20fdf8ae57efe590dde746f9332111e9733c39
Napwob <47683903+Napwob@users.noreply.github.com>
2019-05-26 12:27:56 +0300

    Update string.h

--- string.h
+++ string.h
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int is_valid_ip(char* ip_str);
-int valid_digit(char* ip_str);
-int cip(char* paths);
+void input(char *delim);
+char *sstr(char *string1, const char *string2);
+char* mystrchr(const char* s, const char c);
+int procces(char delim,char* paths);
+int check(char* paths);
 int chs(char *paths);
 int csim(char *paths);
 int cdom(char* paths);
-int check(char* paths);
-int procces(char delim,char* paths);
-void input(char *delim);
+int cip(char* paths);
+int is_valid_ip(char* ip_str);
+int valid_digit(char* ip_str);
 void op(char *paths);
-char *sstr(char *string1, const char *string2);
-char* mystrchr(const char* s, const char c);
 int slen(char *str);
 int sspn(char *str, const char *substr);
 int scmp(const char *str, const char *strc);
